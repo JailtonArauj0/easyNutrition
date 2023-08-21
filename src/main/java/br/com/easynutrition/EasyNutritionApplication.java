9a1903787e1064a8042c8d2f4281df7da47af03e
Jailton Araújo <jailton.flames@gmail.com>
2023-08-21 09:48:41 -0300

    initial project

---
+++ src/main/java/br/com/easynutrition/EasyNutritionApplication.java
@@ -0,0 +1,13 @@
+package br.com.easynutrition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyNutritionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyNutritionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/easynutrition/EasyNutritionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easynutrition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyNutritionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
