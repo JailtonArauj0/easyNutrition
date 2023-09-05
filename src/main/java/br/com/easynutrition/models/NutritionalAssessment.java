fdc387e4fd5002ef4a9022f2fb18b8a14e282c32
Jailton <jailton.flames@gmail.com>
2023-09-05 09:19:03 -0300

    fix embedded class

--- src/main/java/br/com/easynutrition/models/NutritionalAssessment.java
+++ src/main/java/br/com/easynutrition/models/NutritionalAssessment.java
@@ -1,15 +1,25 @@
 package br.com.easynutrition.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
 
 @Embeddable
+@Getter @Setter
 public class NutritionalAssessment{
 
+    @Column(nullable = true)
     private double bmi;
+    @Column(nullable = true)
     private String bmiClassification;
+    @Column(nullable = true)
     private double whr;
+    @Column(nullable = true)
     private String whrClassification;
+    @Column(nullable = true)
     private double bodyFat;
+    @Column(nullable = true)
     private String bfClassification;
 
     //EMBEDDED
