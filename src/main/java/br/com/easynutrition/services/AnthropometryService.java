bd8cff7ffe367338ceba375f7cef6791e4b0c6b7
Jailton <jailton.flames@gmail.com>
2023-09-06 17:09:38 -0300

    add a method to set Nutritional Assessment in a Anthopometry

--- src/main/java/br/com/easynutrition/services/AnthropometryService.java
+++ src/main/java/br/com/easynutrition/services/AnthropometryService.java
@@ -1,7 +1,10 @@
 package br.com.easynutrition.services;
 
 import br.com.easynutrition.models.Anthropometry;
+import br.com.easynutrition.models.NutritionalAssessment;
+import br.com.easynutrition.models.Person;
 import br.com.easynutrition.repositories.AnthropometryRepository;
+import br.com.easynutrition.utils.Classifications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,18 +38,39 @@ public class AnthropometryService {
 
     @Transactional
     public Anthropometry save(Anthropometry anthropometry) {
-        return anthropometryRepository.save(anthropometry);
+        Anthropometry assessment = nutritionalAssessment(anthropometry);
+        return anthropometryRepository.save(assessment);
     }
 
     @Transactional
     public Anthropometry update(Anthropometry anthropometry) {
         anthropometryRepository.findById(anthropometry.getId()).orElseThrow(() -> new RuntimeException("Avaliação não encontrada."));
-        return anthropometryRepository.save(anthropometry);
+        Anthropometry assessment = nutritionalAssessment(anthropometry);
+        return anthropometryRepository.save(assessment);
     }
 
     @Transactional
     public void delete(Long id) {
         anthropometryRepository.findById(id).orElseThrow(() -> new RuntimeException("Avaliação não encontrada."));
         anthropometryRepository.deleteById(id);
+    }
+
+    private Anthropometry nutritionalAssessment(Anthropometry anthropometry) {
+        Classifications classifications = new Classifications(anthropometry);
+        NutritionalAssessment assessment = new NutritionalAssessment();
+
+        Person person = personService.findById(anthropometry.getPerson().getId());
+        classifications.setAge(person.getAge());
+
+        assessment.setBmi(classifications.bmi());
+        assessment.setBmiClassification(classifications.bmiClassification());
+        assessment.setWhr(classifications.whr());
+        assessment.setWhrClassification(classifications.whrClassification());
+        assessment.setBodyFat(classifications.fatPercentagePollock());
+        assessment.setBfClassification(classifications.bfClassification(classifications.fatPercentagePollock()));
+
+        anthropometry.setNutritionalAssessment(assessment);
+
+        return anthropometry;
     }
 }
