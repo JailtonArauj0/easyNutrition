83c165df23231116ee8ba96264ecb013e26a98c8
Jailton <jailton.flames@gmail.com>
2023-09-11 14:34:38 -0300

    created exception classes

---
+++ src/main/java/br/com/easynutrition/exception/CustomException.java
@@ -0,0 +1,12 @@
+package br.com.easynutrition.exception;
+
+import java.io.Serial;
+
+public class CustomException extends RuntimeException{
+    @Serial
+    private static final long serialVersionUID = 2831359556342869402L;
+
+    public CustomException(String message) {
+        super(message);
+    }
+}
