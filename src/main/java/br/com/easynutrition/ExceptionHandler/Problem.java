94c831e6dff4b4955f8feb1c4b28d594e2cbcdae
Jailton <jailton.flames@gmail.com>
2023-09-20 10:12:20 -0300

    Add Exception Handler

---
+++ src/main/java/br/com/easynutrition/ExceptionHandler/Problem.java
@@ -0,0 +1,31 @@
+package br.com.easynutrition.ExceptionHandler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter @Setter
+public class Problem {
+    private Integer status;
+    private String dateTime;
+    private String title;
+    private List<Field> fields;
+
+    public Problem(LocalDateTime localDateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        this.dateTime = localDateTime.format(formatter);
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class Field {
+        private String name;
+        private String message;
+    }
+
+}
