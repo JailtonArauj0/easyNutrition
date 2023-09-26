77c36b7a2659c17979547c8ece3cabd819d1c19f
JailtonArauj0 <jailton.flames@gmail.com>
2023-09-26 16:50:23 -0300

    Create User Repository & Authorization Service

---
+++ src/main/java/br/com/easynutrition/services/AuthorizationService.java
@@ -0,0 +1,19 @@
+package br.com.easynutrition.services;
+
+import br.com.easynutrition.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+    }
+}
