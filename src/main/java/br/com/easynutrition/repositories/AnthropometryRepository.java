e4ad209deb38f673841a90bf7e119206ffb0eac7
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-04-16 15:30:35 -0300

    style: formatação do código

--- src/main/java/br/com/easynutrition/repositories/AnthropometryRepository.java
+++ src/main/java/br/com/easynutrition/repositories/AnthropometryRepository.java
@@ -1,13 +1,11 @@
 package br.com.easynutrition.repositories;
 
 import br.com.easynutrition.models.Anthropometry;
-import br.com.easynutrition.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface AnthropometryRepository extends JpaRepository<Anthropometry, Long> {
@@ -15,8 +13,4 @@ public interface AnthropometryRepository extends JpaRepository<Anthropometry, Lo
     List<Anthropometry> findAllByPersonId(Long id);
 
     List<Anthropometry> findAllByEvaluationDateAndPersonId(LocalDate date, Long id);
-
-    /*
-    @Query("SELECT a FROM Anthropometry a JOIN FETCH a.person")
-    List<Anthropometry> findAllWithPerson(); */
 }
