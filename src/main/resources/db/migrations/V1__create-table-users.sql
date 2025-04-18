84debfba5396853affea39f70296ecfb55a94c26
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-04-18 19:55:43 -0300

    style: ajusta formatação

--- src/main/resources/db/migrations/V1__create-table-users.sql
+++ src/main/resources/db/migrations/V1__create-table-users.sql
@@ -1,11 +1,12 @@
-CREATE TABLE USERS(
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    full_name VARCHAR(255) NOT NULL,
-    cpf VARCHAR(11) NOT NULL UNIQUE,
-    phone VARCHAR(11) DEFAULT NULL,
-    users_role VARCHAR(50) NOT NULL,
+CREATE TABLE USERS
+(
+    id                BIGINT GENERATED ALWAYS AS IDENTITY,
+    email             VARCHAR(255) NOT NULL UNIQUE,
+    password          VARCHAR(255) NOT NULL,
+    full_name         VARCHAR(255) NOT NULL,
+    cpf               VARCHAR(11)  NOT NULL UNIQUE,
+    phone             VARCHAR(11) DEFAULT NULL,
+    users_role        VARCHAR(50)  NOT NULL,
     registration_date TIMESTAMP,
     PRIMARY KEY (id)
 )
--- src/main/resources/db/migrations/V2__create-table-person.sql
+++ src/main/resources/db/migrations/V2__create-table-person.sql
@@ -1,12 +1,13 @@
-CREATE TABLE PERSON(
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) DEFAULT NULL,
-    age INT NOT NULL,
-    phone VARCHAR(11) DEFAULT NULL,
-    cpf VARCHAR(11) NOT NULL UNIQUE,
-    birth_date DATE NOT NULL,
-    nutritionist_id BIGINT NOT NULL
-    CONSTRAINT fk_nutritionist FOREIGN KEY (nutritionist_id) REFERENCES USERS(id),
+CREATE TABLE PERSON
+(
+    id              BIGINT GENERATED ALWAYS AS IDENTITY,
+    first_name      VARCHAR(255) NOT NULL,
+    last_name       VARCHAR(255) DEFAULT NULL,
+    age             INT          NOT NULL,
+    phone           VARCHAR(11)  DEFAULT NULL,
+    cpf             VARCHAR(11)  NOT NULL UNIQUE,
+    birth_date      DATE         NOT NULL,
+    nutritionist_id BIGINT       NOT NULL
+        CONSTRAINT fk_nutritionist FOREIGN KEY (nutritionist_id) REFERENCES USERS(id),
     PRIMARY KEY (id)
 )
