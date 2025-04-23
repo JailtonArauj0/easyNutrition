903bb5427137109a41acc4971555b078aa2101b7
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-04-23 20:41:41 -0300

    refactor: correção na comunicação entre camadas e melhorias gerais nos metodos

--- src/main/java/br/com/easynutrition/controllers/AuthenticationController.java
+++ src/main/java/br/com/easynutrition/controllers/AuthenticationController.java
@@ -2,40 +2,35 @@ package br.com.easynutrition.controllers;
 
 import br.com.easynutrition.configuration.security.TokenService;
 import br.com.easynutrition.dtos.LoginResponseDTO;
-import br.com.easynutrition.dtos.UsersDTO;
-import br.com.easynutrition.dtos.UsersRegisterDTO;
-import br.com.easynutrition.exception.CustomException;
+import br.com.easynutrition.dtos.request.UsersDTO;
+import br.com.easynutrition.dtos.request.UsersRegisterDTO;
 import br.com.easynutrition.models.Users;
-import br.com.easynutrition.repositories.UserRepository;
+import br.com.easynutrition.services.AuthorizationService;
 import jakarta.validation.Valid;
-import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
+    private final AuthorizationService authorizationService;
     private final TokenService tokenService;
 
-    public AuthenticationController(AuthenticationManager authenticationManager, UserRepository userRepository, TokenService tokenService) {
+    public AuthenticationController(AuthenticationManager authenticationManager, AuthorizationService authorizationService, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
+        this.authorizationService = authorizationService;
         this.tokenService = tokenService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid UsersDTO usersDTO) {
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid UsersDTO usersDTO) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(usersDTO.getEmail(), usersDTO.getPassword());
         var auth = authenticationManager.authenticate(usernamePassword);
 
@@ -44,18 +39,8 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody @Valid UsersRegisterDTO usersRegisterDTO) {
-        if (userRepository.findByEmail(usersRegisterDTO.getEmail()).isPresent()) {
-            throw new CustomException("Este e-mail já está em uso");
-        }
-        if (userRepository.findByCpf(usersRegisterDTO.getCpf()).isPresent()) {
-            throw new CustomException("Este CPF já está em uso!");
-        }
-        Users user = new Users();
-        BeanUtils.copyProperties(usersRegisterDTO, user);
-        user.setPassword(new BCryptPasswordEncoder().encode(usersRegisterDTO.getPassword()));
-        user.setRegistrationDate(LocalDateTime.now());
-        userRepository.save(user);
+    public ResponseEntity<?> register(@RequestBody @Valid UsersRegisterDTO usersRegisterDTO) {
+        authorizationService.save(usersRegisterDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
--- src/main/java/br/com/easynutrition/services/AuthorizationService.java
+++ src/main/java/br/com/easynutrition/services/AuthorizationService.java
@@ -1,10 +1,19 @@
 package br.com.easynutrition.services;
 
+import br.com.easynutrition.dtos.request.UsersRegisterDTO;
+import br.com.easynutrition.enums.UsersRole;
+import br.com.easynutrition.exception.CustomException;
+import br.com.easynutrition.models.Users;
 import br.com.easynutrition.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 
 @Service
 public class AuthorizationService implements UserDetailsService {
@@ -17,5 +26,28 @@ public class AuthorizationService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+    }
+
+    public void save(UsersRegisterDTO usersRegisterDTO) {
+        Optional<Users> userExist = userRepository.findByEmailOrCpf(usersRegisterDTO.getEmail(), usersRegisterDTO.getCpf());
+
+        userExist.ifPresent(
+                user -> {
+                    String message = user.getCpf().equals(usersRegisterDTO.getCpf()) ? "Este CPF já está em uso!" : "Este e-mail já está em uso!";
+                    throw new CustomException(message);
+                }
+        );
+
+        Users user = Users.builder()
+                .email(usersRegisterDTO.getEmail())
+                .password(new BCryptPasswordEncoder().encode(usersRegisterDTO.getPassword()))
+                .fullName(usersRegisterDTO.getFullName())
+                .cpf(usersRegisterDTO.getCpf())
+                .phone(usersRegisterDTO.getPhone())
+                .usersRole(usersRegisterDTO.getUsersRole() == null ? UsersRole.USER : usersRegisterDTO.getUsersRole())
+                .registrationDate(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS))
+                .build();
+
+        userRepository.save(user);
     }
 }
