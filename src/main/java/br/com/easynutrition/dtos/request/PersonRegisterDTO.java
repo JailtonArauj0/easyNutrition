09f1456faed8ad3622ffe0cc343e8b48b936fafe
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-08 20:56:10 -0300

    refactor: renomeia classe, ajusta validações e criar mapper para entidade

---
+++ src/main/java/br/com/easynutrition/dtos/request/PersonRegisterDTO.java
@@ -0,0 +1,49 @@
+package br.com.easynutrition.dtos.request;
+
+import br.com.easynutrition.models.Person;
+import br.com.easynutrition.models.Users;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+public class PersonRegisterDTO {
+    @NotBlank(message = "O campo 'firstName' não pode ser nulo.")
+    private String firstName;
+
+    private String lastName;
+
+    @Pattern(regexp = "\\d{11}", message = "O campo 'phone' deve conter 11 dígitos numéricos.")
+    private String phone;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{11}", message = "O campo 'cpf' deve conter 11 dígitos numéricos.")
+    private String cpf;
+
+    @NotNull(message = "O campo 'birthDate' não pode ser nulo.")
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate birthDate;
+
+    @NotNull(message = "O campo 'nutritionistId' não pode ser nulo.")
+    private Long nutritionistId;
+
+    public Person toEntity(Users nutritionist) {
+        int age = LocalDate.now().getYear() - this.getBirthDate().getYear();
+
+        return Person.builder()
+                .firstName(this.getFirstName())
+                .lastName(this.getLastName())
+                .age(age)
+                .phone(this.getPhone())
+                .cpf(this.getCpf())
+                .birthDate(this.getBirthDate())
+                .nutritionist(nutritionist)
+                .build();
+    }
+}
