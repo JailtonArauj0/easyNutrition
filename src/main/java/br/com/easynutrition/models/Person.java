84fd7ac7422e85746fbf4602df1cf72801cdd854
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-08 21:08:47 -0300

    feat: adiciona suporte a builder na classe Person

--- src/main/java/br/com/easynutrition/models/Person.java
+++ src/main/java/br/com/easynutrition/models/Person.java
@@ -3,6 +3,7 @@ package br.com.easynutrition.models;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class Person implements Serializable {
 
     @Serial
