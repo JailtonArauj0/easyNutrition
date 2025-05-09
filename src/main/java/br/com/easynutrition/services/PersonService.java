214cb1961039da1109c93a159d5ff1dcfb2b247d
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-08 21:12:34 -0300

    refactor: melhoria na legibilidade e responsabilidade

--- src/main/java/br/com/easynutrition/services/PersonService.java
+++ src/main/java/br/com/easynutrition/services/PersonService.java
@@ -1,22 +1,27 @@
 package br.com.easynutrition.services;
 
+import br.com.easynutrition.dtos.request.PersonRegisterDTO;
+import br.com.easynutrition.dtos.response.PersonDTO;
 import br.com.easynutrition.exception.CustomException;
 import br.com.easynutrition.exception.EntityNotFoundException;
 import br.com.easynutrition.models.Person;
+import br.com.easynutrition.models.Users;
 import br.com.easynutrition.repositories.PersonRepository;
+import br.com.easynutrition.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PersonService {
     private final PersonRepository personRepository;
+    private final UserRepository userRepository;
 
-    public PersonService(PersonRepository personRepository) {
+    public PersonService(PersonRepository personRepository, UserRepository userRepository) {
         this.personRepository = personRepository;
+        this.userRepository = userRepository;
     }
 
     public List<Person> findAll() {
@@ -27,17 +32,29 @@ public class PersonService {
         return personRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
     }
 
-    @Transactional
-    public Person save(Person person) {
-        Optional<Person> exists = personRepository.findPersonByCpf(person.getCpf());
-        if (exists.isPresent()) {
-            throw new CustomException("Este CPF já está sendo utilizado.");
-        }
-        LocalDate date = person.getBirthDate();
-        int age = LocalDate.now().getYear() - date.getYear();
-        person.setAge(age);
+    private void validateUniqueCpf(String cpf) {
+        personRepository.findPersonByCpf(cpf).ifPresent(
+                person -> {
+                    throw new CustomException("Já existe um usuário cadastrado com o CPF informado.");
+                }
+        );
+    }
 
-        return personRepository.save(person);
+    private Users nutritionistExistAndGet(Long nutritionistId) {
+        return userRepository.findById(nutritionistId)
+                    .orElseThrow(() -> new EntityNotFoundException("Nutricionista não encontrado com o ID informado."));
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public PersonDTO save(PersonRegisterDTO personRegisterDTO) {
+        validateUniqueCpf(personRegisterDTO.getCpf());
+        Users nutritionist = nutritionistExistAndGet(personRegisterDTO.getNutritionistId());
+
+        Person person = personRegisterDTO.toEntity(nutritionist);
+
+        Person personSaved = personRepository.save(person);
+
+        return new PersonDTO(personSaved);
     }
 
     @Transactional
