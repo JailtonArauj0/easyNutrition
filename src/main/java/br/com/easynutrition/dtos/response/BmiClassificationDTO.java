e0380fb257a2dc6e4535cf80542da59e26c0326d
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-26 21:23:19 -0300

    feat: adiciona BmiClassificationDTO para encapsular dados de IMC e classificação

---
+++ src/main/java/br/com/easynutrition/dtos/response/BmiClassificationDTO.java
@@ -0,0 +1,22 @@
+package br.com.easynutrition.dtos.response;
+
+import br.com.easynutrition.models.Anthropometry.Anthropometry;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BmiClassificationDTO {
+    private Double bmi;
+    private String bmiClassification;
+
+    public BmiClassificationDTO(Double bmi, String bmiClassification) {
+        this.bmi = bmi;
+        this.bmiClassification = bmiClassification;
+    }
+
+    public BmiClassificationDTO(Anthropometry anthropometry) {
+        this.bmi = anthropometry.getNutritionalAssessment().getBmi();
+        this.bmiClassification = anthropometry.getNutritionalAssessment().getBmiClassification();
+    }
+}
