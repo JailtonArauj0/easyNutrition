1828b7997b299725655ba55b67cb303f1f4f2d24
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-06-06 20:11:19 -0300

    fix: ajustada função de calculo eer, e construção do objeto

--- src/main/java/br/com/easynutrition/utils/Equations.java
+++ src/main/java/br/com/easynutrition/utils/Equations.java
@@ -1,76 +1,92 @@
 package br.com.easynutrition.utils;
 
-import lombok.Data;
+import br.com.easynutrition.api.exception.CustomException;
+import br.com.easynutrition.domain.enums.Formula;
+import br.com.easynutrition.domain.model.Anthropometry.Anthropometry;
+import br.com.easynutrition.domain.model.CaloricExpenditure.CaloricExpenditure;
+import br.com.easynutrition.domain.model.Person.Person;
+import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-
-@Data
+@Getter
+@Setter
 public class Equations {
-
     private Double weight;
     private Double height;
     private Integer age;
-    private Character sex;
+    private String sex;
     private Double activityFactor;
     private Double geb;
     private Double get;
+    private Person person;
 
-
-    public void eerIom(char sex) {
-
-        if (sex == 'M') {
-            if (this.activityFactor == 1.2) {
-                this.get = 753.07 - (10.83 * age) + (6.50 * height) + (14.10 * weight);
-
-            } else if (this.activityFactor == 1.375) {
-                this.get = 581.47 - (10.83 * age) + (8.30 * height) + (14.94 * weight);
-
-            } else if (this.activityFactor == 1.55) {
-                this.get = 1004.82 - (10.83 * age) + (6.52 * height) + (15.91 * weight);
-
-            } else if (this.activityFactor == 1.725) {
-                this.get = -517.88 - (10.83 * age) + (15.61 * height) + (19.11 * weight);
-
-            }
-        } else {
-            if (this.activityFactor == 1.2) {
-                this.get = 584.90 - (7.01 * age) + (5.72 * height) + (11.71 * weight);
-
-            } else if (this.activityFactor == 1.375) {
-                this.get = 575.77 - (7.01 * age) + (6.60 * height) + (12.14 * weight);
-
-            } else if (this.activityFactor == 1.55) {
-                this.get = 710.25 - (7.01 * age) + (6.54 * height) + (12.34 * weight);
-
-            } else if (this.activityFactor == 1.725) {
-                this.get = 511.83 - (7.01 * age) + (9.07 * height) + (12.56 * weight);
-
-            }
-        }
+    public Equations(Anthropometry anthropometry) {
+        this.weight = anthropometry.getWeight();
+        this.height = (double) anthropometry.getHeight() / 100;
+        this.age = anthropometry.getPerson().getAge();
+        this.sex = anthropometry.getSex().name();
+        this.person = anthropometry.getPerson();
     }
 
-    public void harrisBenedict(char sex) {
+    public CaloricExpenditure eerIom(String sex) {
+        if (sex.equalsIgnoreCase("Masculino")) {
+            this.get = 662 - (9.53 * age) + activityFactor * ((15.91 * weight) + (539.6 * height));
 
-        if (sex == 'M') {
-            this.geb = 66.5 + (13.75 * weight) + (5.003 * height) - (6.75 * age);
+        } else if (sex.equalsIgnoreCase("Feminino")) {
+            this.get = 354 - (6.91 * age) + activityFactor * ((9.36 * weight) + (726 * height));
 
         } else {
-            this.geb = 655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age);
-
+            throw new CustomException("Sexo deve ser masculino ou feminino");
         }
-        this.get = geb * activityFactor;
+        this.geb = 0.0;
+
+        CaloricExpenditure caloricExpenditure = new CaloricExpenditure();
+        caloricExpenditure.equationToEntity(this);
+        caloricExpenditure.setFormula(Formula.EER_IOM);
+        return caloricExpenditure;
         /*
-        Sedentary: 1.2
-        Lightly active: 1.375
-        Moderately active ~ Active: 1.55
-        Very active: 1.725
+        MASCULINO
+        Sedentário	1.00
+        Pouco ativo	1.11
+        Ativo	1.25
+        Muito ativo	1.48
+
+        FEMININO
+        Sedentário	1.00
+        Pouco ativa	1.12
+        Ativa	1.27
+        Muito ativa	1.45
         */
     }
 
-    public void mifflin(char sex) {
+    public CaloricExpenditure harrisBenedict(String sex) {
 
-        if (sex == 'M') {
+        if (sex.equalsIgnoreCase("Masculino")) {
+            this.geb = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+
+        } else {
+            this.geb = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+
+        }
+        this.get = geb * activityFactor;
+
+        CaloricExpenditure caloricExpenditure = new CaloricExpenditure();
+        caloricExpenditure.equationToEntity(this);
+        caloricExpenditure.setFormula(Formula.HARRIS_BENEDICT);
+        return caloricExpenditure;
+
+        /*
+        Sedentário: 1.2
+        Levemente ativo: 1.375
+        Moderadamente ativo: 1.55
+        Muito ativo: 1.725
+        Extremamente ativo: 1.9
+        */
+    }
+
+    public CaloricExpenditure mifflin(String sex) {
+
+        if (sex.equalsIgnoreCase("Masculino")) {
             this.geb = (10 * weight) + (6.25 * height) - (5.0 * age) + 5;
 
         } else {
@@ -78,5 +94,10 @@ public class Equations {
 
         }
         this.get = geb * activityFactor;
+
+        CaloricExpenditure caloricExpenditure = new CaloricExpenditure();
+        caloricExpenditure.equationToEntity(this);
+        caloricExpenditure.setFormula(Formula.MIFFLIN_ST_JEOR);
+        return caloricExpenditure;
     }
 }
