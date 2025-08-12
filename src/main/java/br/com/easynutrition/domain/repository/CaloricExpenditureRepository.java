62d38fc46d48cb1dd2bdec0eb894d31cdd23441c
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-08-12 15:31:43 -0300

    fix: ajustado retorno do metodo

--- src/main/java/br/com/easynutrition/domain/repository/CaloricExpenditureRepository.java
+++ src/main/java/br/com/easynutrition/domain/repository/CaloricExpenditureRepository.java
@@ -2,7 +2,12 @@ package br.com.easynutrition.domain.repository;
 
 import br.com.easynutrition.domain.model.CaloricExpenditure.CaloricExpenditure;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface CaloricExpenditureRepository extends JpaRepository<CaloricExpenditure, Long> {
-    public CaloricExpenditure findAllByPersonId(Long id);
+    Optional<List<CaloricExpenditure>> findAllByPersonId(Long id);
 }
