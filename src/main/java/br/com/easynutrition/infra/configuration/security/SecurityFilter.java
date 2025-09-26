686d2856fab448fd467349d8454f2d0fbd3b5379
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-09-26 16:41:10 -0300

    fix: adicionado verificação de nulidade evitando nullpointerException

--- src/main/java/br/com/easynutrition/infra/configuration/security/SecurityFilter.java
+++ src/main/java/br/com/easynutrition/infra/configuration/security/SecurityFilter.java
@@ -1,7 +1,7 @@
 package br.com.easynutrition.infra.configuration.security;
 
-import br.com.easynutrition.domain.model.User.Users;
 import br.com.easynutrition.domain.repository.UserRepository;
+import br.com.easynutrition.domain.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Optional;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -28,11 +27,11 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if (token != null) {
-            var subject = tokenService.validateToken(token);
-            Optional<Users> user = userRepository.findByEmail(subject);
-
-            var authentication = new UsernamePasswordAuthenticationToken(user.get(), null, user.get().getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            var subject = tokenService.validateToken(token, request);
+            userRepository.findByEmail(subject).ifPresent((userAuthenticated) -> {
+                var authentication = new UsernamePasswordAuthenticationToken(userAuthenticated, null, userAuthenticated.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            });
         }
         filterChain.doFilter(request, response);
     }
