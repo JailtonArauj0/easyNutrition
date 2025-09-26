3b439c8d5b2dcac8af69408495a961cefdbb257e
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-09-26 16:45:11 -0300

    refactor: removido optional da consulta

--- src/main/java/br/com/easynutrition/domain/repository/CaloricExpenditureRepository.java
+++ src/main/java/br/com/easynutrition/domain/repository/CaloricExpenditureRepository.java
@@ -5,9 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CaloricExpenditureRepository extends JpaRepository<CaloricExpenditure, Long> {
-    Optional<List<CaloricExpenditure>> findAllByPersonId(Long id);
+    List<CaloricExpenditure> findAllByPersonId(Long id);
 }
