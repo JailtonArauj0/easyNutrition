e45308f89f011fc7f7681daec07e1d402e1b22a9
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-09-26 17:22:17 -0300

    test: adiciona classe main de teste

--- src/test/java/br/com/easynutrition/EasyNutritionApplicationTests.java
+++ src/test/java/br/com/easynutrition/EasyNutritionApplicationTests.java
@@ -1,13 +1,8 @@
 package br.com.easynutrition;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EasyNutritionApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
